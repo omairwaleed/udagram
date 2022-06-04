84dd3482b5554b20a8c4e207b99d3f963e456340
omairwaleed <omairwaleed17gmail.com>
2022-06-04 15:44:33 +0200

    14 change

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -4,10 +4,4 @@ cd www
 printenv > .env
 eb init --region $AWS_REGION $EP_APP
 eb deploy $EP_ENV
-eb setenv JWT_SECRET=omair
-eb setenv PORT=3000
-eb setenv POSTGRES_DB=udagram
-eb setenv POSTGRES_HOST=database-1.cppgss9en3mm.us-east-1.rds.amazonaws.com
-eb setenv POSTGRES_PASSWORD=omairwaleed
-eb setenv POSTGRES_USERNAME=postgres
-eb setenv db_port=5432
+
